d00cdf547cb3f27e44653d183d82167007681c21
chano64123 <promo201674@gmail.com>
2023-03-14 23:00:19 -0500

    data annotations

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,8 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace projectEF.Models;
 
+[Table("Category")]
 public class Category {
+    [Key]
     public Guid IdCategory { get; set; }
+    
+    [Required]
+    [MaxLength(150)]
+    [MinLength(5)]
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
+
     public virtual ICollection<Task> Tasks { get; set; } = new List<Task>();
 }
--- Models/Task.cs
+++ Models/Task.cs
@@ -1,12 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace projectEF.Models;
 
+[Table("Task")]
 public class Task {
+    [Key]
     public Guid IdTask { get; set; }
+
+    [ForeignKey("IdCategory")]
     public Guid IdCategory { get; set; }
+
+    [Required]
+    [MaxLength(200)]
+    [MinLength(10)]
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public Priority Priority { get; set; }
     public DateTime DateCreate { get; set; }
+
+    [NotMapped]
+    public string Resume { get; set; } = string.Empty;
+
     public virtual Category Category { get; set; } = new Category();
 }
 
