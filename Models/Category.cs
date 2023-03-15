26915cc64eed885c610b6040205d4d6f84fab61e
chano64123 <promo201674@gmail.com>
2023-03-14 22:33:10 -0500

    category model

---
+++ Models/Category.cs
@@ -0,0 +1,7 @@
+namespace projectEF.Models;
+
+public class Category {
+    public Guid IdCategory { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+}
